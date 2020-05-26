9c9b34d03cffe7cc62180b6b46f3dab3a8eeeca7
Victor <youdom.k.v@gmail.com>
2020-05-26 13:27:03 -0700

    registration of stakers fixed. Still issues with unstaking method

--- include/tropiumstake.hpp
+++ include/tropiumstake.hpp
@@ -1,11 +1,14 @@
-/*
-Author: Victor
-Github: https://github.com/VicXekro
+/**
+* Staking smart contract for the Tropiumchain.io 
+* Author: Victor
+* Github: https://github.com/VicXekro
 */
 
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
 #include <eosio/system.hpp>
+#include <eosio/transaction.hpp>
+#include <eosio.token.hpp>
 
 using namespace std;
 using namespace eosio;
@@ -16,45 +19,61 @@ CONTRACT tropiumstake : public contract {
 
      tropiumstake(name receiver, name code, datastream<const char*> ds):
         contract(receiver, code, ds),
-        _staker_list(receiver, receiver.value){}
+        _staker_list(receiver, receiver.value),
+        tropium_symb("TRPM",0){} //thre precision of the symbol is the decimal precision of
+                                  //the created token
 
-    /*
-      Register the user and stake its amounts
-      --> if user already registered, send error message
+    /**
+    * Register a user in the list of stakers.
+    * @param: username, the account name of the staker
     */
-    [[eosio::on_notify("eosio.token::transfer")]]
-    void regstake(name username, name receiver, asset quantity, string memo);
+    ACTION regstaker(name username);
 
-    /*
-    Increase the stake of a registered user by the precised quantity
-    --> if user not registered, send error message
+    /**
+    * Catch the transfer from eosio.token and register the funds staked
+    * @param: receiver, the account owning this smart contract
+    * @param: msg, define the action to be used. "start" to deposit the first funds, and 
+    *         "increment" to increase the staked funds 
     */
-    [[eosio::on_notify("eosio.token::transfer")]]
-    void incrstake(name username, name receiver, asset quantity, string memo);
+    [[eosio::on_notify("*::transfer")]]
+    void stake (name username, name receiver, asset quantity, string msg);
+
+    /**
+    * Check the "standing" of a staker in our staking smart contract
+    * i.e., if the account is registered or not, if yes, then show the staked amount. Also check if account is banned
+    * @param: username, the account whose standing is checked.
+    */
+    //ACTION checkstaker (name username);
 
     /*
-    Cancel the stake of a user and remove him from the table, refund the held amount
+    * Cancel the stake of a user and remove him from the table, refund the held amount
+    * @param: username, the account wanting to unstake its funds. 
     */
     ACTION unstake (name username);
 
-    /*
-    Check the amount staked by username
-    */
-    ACTION checkstake(name username);
-
   private:
-    // table used to maintain the list of staking nodes
+      // table used to maintain the list of staking nodes
         TABLE staker{
-        name username; //name of the staking user
-        asset fund_staked; // funds to be staked
-        uint64_t primary_key() const {return username.value;}
+          name username; //name of the staking user
+          asset fund_staked; // funds to be staked
+          bool isstaked; //if the users has already staked funds or not
+          uint64_t primary_key() const {return username.value;}
         };
 
         typedef eosio::multi_index<name("stakerlist"), staker> stakers;
 
         stakers _staker_list;
 
-        //the symbol of the tropium token
-        //TODO: define this variable
+        //the symbol of the tropium token (TRPM, 0.0000)
         const symbol tropium_symb;
+
+        /*// table used to maintain banned people. Doctors approval needed to remove them from the list
+        TABLE banned{
+          name username;
+          uint64_t primary_key() const {return username.value;}
+        }*/
+
+        //void isbanned(name username);
+
+
 };
--- src/tropiumstake.cpp
+++ src/tropiumstake.cpp
@@ -1,5 +1,46 @@
 #include <tropiumstake.hpp>
 
+ACTION tropiumstake::regstaker (name username){
+  require_auth(username);
+  auto itr = _staker_list.find(username.value);
+
+  check(itr == _staker_list.end(), "You are already registered, you can stake your TRPM");
+
+  asset temp_asset(0, tropium_symb);
+  itr = _staker_list.emplace(username, [&](auto& row){
+    row.username = username;
+    row.fund_staked = temp_asset;
+    row.isstaked = false;
+  });
+
+}
+
+void tropiumstake::stake(name username, name receiver, asset quantity, string msg){
+  require_auth(username);
+  if (receiver != get_self() || username == get_self()) return;
+  auto itr = _staker_list.find(username.value);
+
+  check(itr != _staker_list.end(), "You cannot stake, you are not yet registered");
+  check(quantity.symbol == tropium_symb, "wrong token used");
+  check(msg == "increment" || msg =="start", "Please use \"increment\" to increase your stake or \"start\" to deposit your first stake");
+  
+  if(msg == "start" && itr->isstaked == false){
+  check(quantity.amount >= 5000, "Staked amount not enough, stake at least 5000");
+    _staker_list.modify(itr, username, [&](auto& row){
+      row.fund_staked = quantity;
+      row.isstaked = true;
+    });
+  }
+  else if (msg == "increment" && itr->isstaked == true){
+    _staker_list.modify(itr, username, [&](auto& row){
+      row.fund_staked += quantity;
+    });
+  }
+  else check(false, "Error with staking options, please check you status");
+
+  require_recipient(username);
+}
+/*
 void tropiumstake::regstake(name username, name receiver, asset quantity, string memo) {
   // enforce user authentication
   if (receiver != get_self() || username == get_self()) return;
@@ -10,8 +51,9 @@ void tropiumstake::regstake(name username, name receiver, asset quantity, string
   //check if not already in staker list
   check(itr == _staker_list.end(), "You are already a staker, check you staked amount instead");
   
-  //check if quantity given is at least the minimum required
+  //check if quantity given is at least the minimum required and required currency
   check(quantity.amount >= 5000, "Staked amount not enough, stake at least 5000");
+  check(quantity.symbol == tropium_symb, "wrong token used");
 
   //TODO: to modify syntax below to check with tropium_symb
   //check(quantity.symbol == hodl_symbol, "These are not the droids you are looking for.");
@@ -21,6 +63,7 @@ void tropiumstake::regstake(name username, name receiver, asset quantity, string
     row.username = username;
     row.fund_staked = quantity;
   });
+  //require_recipient(username);
 }
 
 void tropiumstake::incrstake(name username, name receiver, asset quantity, string memo){
@@ -34,43 +77,33 @@ void tropiumstake::incrstake(name username, name receiver, asset quantity, strin
  //check if not already in staker list
   check(itr != _staker_list.end(), "Your are not a staker, please register first");
 
+  //check if quantity given is valid
+  check(quantity.is_valid() && (quantity.symbol == tropium_symb), "Given amount is not valid");
+
   //increment amount stake
    _staker_list.modify(itr, username, [&]( auto& row ) {
       row.username = username;
       row.fund_staked += quantity;
     });
 
-}
+}*/
 
-ACTION tropiumstake:: unstake (name username){
-  //enforce user authentication
+ACTION tropiumstake::unstake (name username){
   require_auth(username);
-
-   // set iterator on table
   auto itr = _staker_list.find(username.value);
-
-  //check if not already in staker list
   check(itr != _staker_list.end(), "Your are not a staker, please register first");
+  print(get_self());
+
+  //TODO: Chance contract name to eosio.token during deployment
+  //token::transfer_action payout("test1"_n, {get_self(), "active"_n});
+  //payout.send(get_self(), username, itr->fund_staked, "your funds have been released");
 
   action{
         permission_level{get_self(), "active"_n},
-        "eosio.token"_n,
+        "test1"_n,
         "transfer"_n,
         std::make_tuple(get_self(), username, itr->fund_staked, std::string("Your fund have been released"))
       }.send();
 }
-
-EOSIO_DISPATCH(tropiumstake, (regstake)(incrstake)(unstake))
-/*ACTION tropiumstake::clear() {
-  require_auth(get_self());
-
-  messages_table _messages(get_self(), get_self().value);
-
-  // Delete all records in _messages table
-  auto msg_itr = _messages.begin();
-  while (msg_itr != _messages.end()) {
-    msg_itr = _messages.erase(msg_itr);
-  }
-}
-
-EOSIO_DISPATCH(tropiumstake, (hi)(clear))*/
+//_staker_list.erase(itr);
+//std::make_tuple(get_self(), username, itr->fund_staked, std::string("Your fund have been released"))
